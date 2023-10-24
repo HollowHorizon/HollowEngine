f15824a71bb637a2d8d74b0815e53abca5e540f2
Ivan <46783751+AlgorithmLX@users.noreply.github.com>
2023-10-24 10:03:47 +0700

    No comment, thanks Git for bug

---
+++ src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/nodes/npcs/NpcHelper.kt
@@ -0,0 +1,6 @@
+package ru.hollowhorizon.hollowengine.common.scripting.story.nodes.npcs
+
+import ru.hollowhorizon.hollowengine.common.entities.NPCEntity
+import ru.hollowhorizon.hollowengine.common.npcs.NPCSettings
+import ru.hollowhorizon.hollowengine.common.npcs.SpawnLocation
+
