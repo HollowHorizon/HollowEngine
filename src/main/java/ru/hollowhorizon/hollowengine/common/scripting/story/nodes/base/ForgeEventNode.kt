b82dc3e3a273d8bd2f2819837c8f74f6f8ebe9a7
Ivan <46783751+AlgorithmLX@users.noreply.github.com>
2023-11-11 21:19:08 +0700

    Try to fix error

--- src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/nodes/base/ForgeEventNode.kt
+++ src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/nodes/base/ForgeEventNode.kt
@@ -2,17 +2,10 @@ package ru.hollowhorizon.hollowengine.common.scripting.story.nodes.base
 
 import net.minecraft.nbt.CompoundTag
 import net.minecraftforge.common.MinecraftForge
-import net.minecraftforge.event.ServerChatEvent
 import net.minecraftforge.eventbus.api.Event
 import net.minecraftforge.eventbus.api.SubscribeEvent
-import ru.hollowhorizon.hollowengine.common.entities.NPCEntity
-import ru.hollowhorizon.hollowengine.common.npcs.NPCSettings
-import ru.hollowhorizon.hollowengine.common.npcs.SpawnLocation
 import ru.hollowhorizon.hollowengine.common.scripting.story.nodes.IContextBuilder
 import ru.hollowhorizon.hollowengine.common.scripting.story.nodes.Node
-import kotlin.properties.ReadWriteProperty
-import kotlin.reflect.KProperty
-
 
 
 open class ForgeEventNode<T : Event>(private val type: Class<T>, open val action: (T) -> Boolean) : Node() {
