508e1bf1e0f078d771809c340e37e87fc4b3e720
HollowHorizon <78298979+HollowHorizon@users.noreply.github.com>
2023-12-24 09:45:50 +0300

    message in chat waiting

--- src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/nodes/base/events/InputNode.kt
+++ src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/nodes/base/events/InputNode.kt
@@ -2,14 +2,18 @@ package ru.hollowhorizon.hollowengine.common.scripting.story.nodes.base.events
 
 import net.minecraft.nbt.CompoundTag
 import net.minecraftforge.event.ServerChatEvent
+import ru.hollowhorizon.hollowengine.common.scripting.story.nodes.IContextBuilder
 import ru.hollowhorizon.hollowengine.common.scripting.story.nodes.base.ForgeEventNode
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
+fun IContextBuilder.input(vararg values: String, onlyHostMode: Boolean = false) =
+    +InputNode(*values, onlyHostMode = onlyHostMode)
+
 class InputNode(vararg val values: String, val onlyHostMode: Boolean = false) :
-    ForgeEventNode<ServerChatEvent>(ServerChatEvent::class.java, { true }), ReadWriteProperty<Any?, () -> String> {
+    ForgeEventNode<ServerChatEvent.Submitted>(ServerChatEvent.Submitted::class.java, { true }), ReadWriteProperty<Any?, String> {
     var message: String = "Nothing"
-    override val action = { event: ServerChatEvent ->
+    override val action = { event: ServerChatEvent.Submitted ->
         val isFromTeam =
             event.player in manager.team.onlineMembers || (onlyHostMode && event.player.uuid == manager.team.owner)
         if (isFromTeam) message = event.message.string
@@ -27,11 +31,11 @@ class InputNode(vararg val values: String, val onlyHostMode: Boolean = false) :
         message = nbt.getString("message")
     }
 
-    override fun getValue(thisRef: Any?, property: KProperty<*>): () -> String {
-        return { message }
+    override fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        return message
     }
 
-    override fun setValue(thisRef: Any?, property: KProperty<*>, value: () -> String) {
-        message = value()
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+        message = value
     }
 }
