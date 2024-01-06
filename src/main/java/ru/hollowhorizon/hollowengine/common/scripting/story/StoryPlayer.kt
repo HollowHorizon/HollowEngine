c2326c4de9164d4259f8d171af11d369a648f415
HollowHorizon <artemm@xaker.ru>
2024-01-06 14:15:12 +0300

    random pos

--- src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/StoryPlayer.kt
+++ src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/StoryPlayer.kt
@@ -1,7 +1,29 @@
 package ru.hollowhorizon.hollowengine.common.scripting.story
 
+import dev.ftb.mods.ftbteams.data.Team
 import net.minecraft.world.entity.player.Player
+import net.minecraft.world.level.levelgen.Heightmap
+import net.minecraft.world.phys.Vec3
+import ru.hollowhorizon.hc.HollowCore
+import ru.hollowhorizon.hollowengine.HollowEngine
+import kotlin.math.abs
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.sin
 
 private val Player.scriptName: String?
     get() = if (this.persistentData.contains("hs_name")) this.persistentData.getString("hs_name") else null
 
+fun Team.randomPos(radius: Float = 10f): Vec3 {
+    val player = onlineMembers.random()
+    val pos = player.position()
+    val randomAngle = Math.toRadians(Math.random() * 360.0).toFloat()
+
+    val xOffset = radius * cos(randomAngle)
+    val zOffset = radius * sin(randomAngle)
+
+    val y =
+        player.level.getHeight(Heightmap.Types.WORLD_SURFACE_WG, (pos.x + xOffset).toInt(), (pos.z + zOffset).toInt())
+
+    return Vec3(pos.x + xOffset, y.toDouble(), pos.z + zOffset)
+}
