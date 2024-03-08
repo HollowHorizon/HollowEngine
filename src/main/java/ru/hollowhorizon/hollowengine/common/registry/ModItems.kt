ab69cba22166bf3dd58aead651369bfaa728de4f
HollowHorizon <artemm@xaker.ru>
2024-03-08 15:40:39 +0300

    camera model fix

--- src/main/java/ru/hollowhorizon/hollowengine/common/registry/ModItems.kt
+++ src/main/java/ru/hollowhorizon/hollowengine/common/registry/ModItems.kt
@@ -1,12 +1,13 @@
 package ru.hollowhorizon.hollowengine.common.registry
 
 import ru.hollowhorizon.hc.common.registry.HollowRegistry
+import ru.hollowhorizon.hc.common.registry.ObjectConfig
 import ru.hollowhorizon.hollowengine.common.items.CameraItem
 import ru.hollowhorizon.hollowengine.common.items.NpcTool
 import ru.hollowhorizon.hollowengine.common.items.StoryTellerDimItem
 
 object ModItems : HollowRegistry() {
     val STORYTELLER_DIM_TELEPORTER by register("storyteller_dim_teleporter", ::StoryTellerDimItem)
-    val CAMERA by register("camera", ::CameraItem)
+    val CAMERA by register(ObjectConfig(name = "camera", autoModel = false), ::CameraItem)
     val NPC_TOOL by register("npc_tool", ::NpcTool)
 }
