0ecf47ea45c2997e53df1c5d8f007407feef285e
HollowHorizon <artemm@xaker.ru>
2024-05-17 11:55:00 +0300

    storyteller dimension fix

--- src/main/java/ru/hollowhorizon/hollowengine/mixins/DimensionTypeMixin.java
+++ src/main/java/ru/hollowhorizon/hollowengine/mixins/DimensionTypeMixin.java
@@ -24,24 +24,28 @@
 
 package ru.hollowhorizon.hollowengine.mixins;
 
-import net.minecraft.world.level.chunk.storage.ChunkStorage;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraftforge.fml.loading.FMLPaths;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import ru.hollowhorizon.hc.HollowCore;
 
 import java.nio.file.Path;
 
-@Mixin(ChunkStorage.class)
-public class ChunkLoaderMixin {
-    @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/storage/IOWorker;<init>(Ljava/nio/file/Path;ZLjava/lang/String;)V"))
-    private Path constructor(Path pFolder) {
-        String dim = pFolder.getParent().toFile().getName();
+@Mixin(DimensionType.class)
+public class DimensionTypeMixin {
+    @Inject(method = "getStorageFolder", at = @At("RETURN"), cancellable = true)
+    private static void onGetWorldFolder(ResourceKey<Level> pDimensionKey, Path pLevelFolder, CallbackInfoReturnable<Path> cir) {
 
-        HollowCore.LOGGER.info("Redirect chunk loader: {}", dim);
 
-        if(dim.equals("storyteller_dimension")) return FMLPaths.GAMEDIR.get().resolve("hollowengine").resolve("storyteller_world");
-        else return pFolder;
+        if (pDimensionKey.location().equals(new ResourceLocation("hollowengine:storyteller_dimension"))) {
+            HollowCore.LOGGER.info("Redirect StoryTeller dimension folder!");
+            cir.setReturnValue(FMLPaths.GAMEDIR.get().resolve("hollowengine").resolve("storyteller_world"));
+        }
     }
 }
