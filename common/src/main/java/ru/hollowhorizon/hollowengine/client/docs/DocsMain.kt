136ca4417f406d2babbb7457a52270e27ac38d98
_BENDY659_ <denisglazyrin13@gmail.com>
2024-08-09 19:01:45 +0900

    :( :apple:

---
+++ common/src/main/java/ru/hollowhorizon/hollowengine/client/docs/DocsMain.kt
@@ -0,0 +1,12 @@
+package ru.hollowhorizon.hollowengine.client.docs
+
+import imgui.ImGui
+
+@DocsPage("hc")
+fun DocsRenderer.hc() {
+  ImGui.text("HollowCore")
+}
+@DocsPage("he")
+fun DocsRenderer.he() {
+  ImGui.text("HollowEngine")
+}
