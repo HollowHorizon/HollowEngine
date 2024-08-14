d44cf62973d62f1e30df5c13586f3097efc7caa5
_BENDY659_ <denisglazyrin13@gmail.com>
2024-08-14 12:35:36 +0900

    Starting Volume "Story Events" | Starting SubCVolume "NPC" | Starting Chapter "Create"

---
+++ common/src/main/java/ru/hollowhorizon/hollowengine/client/docs/pages/story_events/npc/ANpc.kt
@@ -0,0 +1,10 @@
+package ru.hollowhorizon.hollowengine.client.docs.pages.story_events.npc
+
+import imgui.ImGui
+import ru.hollowhorizon.hollowengine.client.docs.DocsPage
+import ru.hollowhorizon.hollowengine.client.docs.DocsRenderer
+
+@DocsPage("1_he.2_story_events.0_npc")
+fun DocsRenderer.pNpc() {
+  ImGui.text("NPC")
+}
