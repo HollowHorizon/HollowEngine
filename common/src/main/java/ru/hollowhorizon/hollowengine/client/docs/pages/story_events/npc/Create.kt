f3f9c73d79d04a053c78a85959f064494c1cc331
_BENDY659_ <denisglazyrin13@gmail.com>
2024-08-14 18:06:17 +0900

    ХАХАХАХАХХАХА Блин.. Какой "Глава Begin окончена", я про всё остальное забыл XDDDD))))))

--- common/src/main/java/ru/hollowhorizon/hollowengine/client/docs/pages/story_events/npc/Create.kt
+++ common/src/main/java/ru/hollowhorizon/hollowengine/client/docs/pages/story_events/npc/Create.kt
@@ -1,9 +1,18 @@
 package ru.hollowhorizon.hollowengine.client.docs.pages.story_events.npc
 
+import imgui.ImGui
 import ru.hollowhorizon.hollowengine.client.docs.DocsPage
 import ru.hollowhorizon.hollowengine.client.docs.DocsRenderer
+import ru.hollowhorizon.hollowengine.client.docs.DocsUtils.text
+import ru.hollowhorizon.hollowengine.client.docs.DocsUtils.titleImg
 
-@DocsPage("1_he.2_story_events.0_npc.0_create")
+const val a = "1_he.2_story_events.0_npc.0_create"
+
+@DocsPage(a)
 fun DocsRenderer.npcCreate() {
+  text("$a.text0", 50)
 
+  ImGui.newLine()
+
+  titleImg("")
 }
