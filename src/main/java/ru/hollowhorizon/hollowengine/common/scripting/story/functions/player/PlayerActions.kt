b7d13d8f03bf0781e15c1b32c135f61e833b20ee
HollowHorizon <artemm@xaker.ru>
2024-11-05 20:46:47 +0300

    Update PlayerActions.kt

--- src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/functions/player/PlayerActions.kt
+++ src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/functions/player/PlayerActions.kt
@@ -1,7 +1,12 @@
 package ru.hollowhorizon.hollowengine.common.scripting.story.functions.player
 
+import net.minecraft.ChatFormatting
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.phys.Vec3
+import ru.hollowhorizon.hc.client.utils.colored
+import ru.hollowhorizon.hc.client.utils.literal
+import ru.hollowhorizon.hc.client.utils.plus
+import ru.hollowhorizon.hollowengine.client.gui.scripting.sendToast
 import ru.hollowhorizon.hollowengine.compiler.suspendable.await
 import ru.hollowhorizon.hollowengine.scripting.Suspendable
 
@@ -13,3 +18,9 @@ fun Player.waitPos(pos: Vec3, radius: Float = 1f, inverse: Boolean = false) {
         await(distanceToSqr(pos) <= radius * radius)
     }
 }
+
+fun Player.say(text: String) =
+    sendSystemMessage("[".literal.colored(ChatFormatting.GOLD) + name + "] ".literal.colored(ChatFormatting.GOLD) + text.literal)
+
+fun Player.send(text: String) = sendSystemMessage(text.literal)
+fun Player.notify(text: String) = sendToast(text.literal)
