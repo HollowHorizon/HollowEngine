516e4ec3d30d53542e6a4e091a4914d4e585f9ce
HollowHorizon <artemm@xaker.ru>
2025-01-30 10:48:05 +0300

    include hollowcore in mod

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ repositories {
 }
 
 dependencies {
-    install("ru.hollowhorizon:HollowCore-${container.modPlatform}-${container.minecraftVersion}:$hollowcore:dev", includeInJar = false)
+    install("ru.hollowhorizon:HollowCore-${container.modPlatform}-${container.minecraftVersion}:$hollowcore:dev", includeInJar = true)
 
     setupScripting()
 
