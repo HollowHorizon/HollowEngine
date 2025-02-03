ad91421f74377e9c7c790191b6a552f30b7c7f4b
HollowHorizon <artemm@xaker.ru>
2025-02-03 14:30:04 +0300

    Update HollowScriptConfiguration.kt

--- src/main/java/ru/hollowhorizon/hollowengine/common/scripting/core/configuration/HollowScriptConfiguration.kt
+++ src/main/java/ru/hollowhorizon/hollowengine/common/scripting/core/configuration/HollowScriptConfiguration.kt
@@ -39,7 +39,7 @@ open class HollowScriptConfiguration(body: Builder.() -> Unit = {}) : ScriptComp
         *///?}
 
         updateClasspath(jars + filteredClasspath)
-        if(true || !isProduction) dependenciesFromCurrentContext(wholeClasspath = true)
+        if(!isProduction) dependenciesFromCurrentContext(wholeClasspath = true)
     }
 
     defaultImports(Import::class)
