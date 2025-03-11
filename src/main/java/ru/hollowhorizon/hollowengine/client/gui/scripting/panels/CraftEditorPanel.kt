33eab3d7f70787f802718f9c2ebb8d8278c29e11
bat1set <daniil.fisenko.ps@gmail.com>
2025-03-12 03:06:15 +0800

    update to hollow

--- src/main/java/ru/hollowhorizon/hollowengine/client/gui/scripting/panels/CraftEditorPanel.kt
+++ src/main/java/ru/hollowhorizon/hollowengine/client/gui/scripting/panels/CraftEditorPanel.kt
@@ -4,15 +4,8 @@ import net.minecraft.client.Minecraft
 import de.fabmax.kool.modules.ui2.*
 import de.fabmax.kool.modules.ui2.Button
 import de.fabmax.kool.modules.ui2.docking.Dock
-import de.fabmax.kool.modules.ui2.docking.DockNode
 import net.minecraft.world.item.crafting.Recipe
 import net.minecraft.world.item.crafting.RecipeType
-import ru.hollowhorizon.hollowengine.client.gui.docs.DocsNode
-import ru.hollowhorizon.hollowengine.client.gui.scripting.FileNode
-import ru.hollowhorizon.hollowengine.client.gui.scripting.IDEStorage.dock
-import ru.hollowhorizon.hollowengine.client.gui.scripting.IDEStorage.files
-import ru.hollowhorizon.hollowengine.client.gui.scripting.docking.insertItem
-import ru.hollowhorizon.hollowengine.client.gui.scripting.files.DocFileData
 
 
 class CraftEditorPanel(dock: Dock) : DockPanel("hollowengine.gui.ide.craft_editor", dock) {
@@ -65,17 +58,6 @@ class CraftEditorPanel(dock: Dock) : DockPanel("hollowengine.gui.ide.craft_edito
     fun addRecipeTypes() {
         recipeList.forEach { recipeTypes.add(it.type) }
     }
-
-    fun openDocFile(node: FileNode) {
-        val page = (node as? DocsNode)?.page ?: return
-        files.getOrPut(node.treePath) {
-            val localFile = DocFileData(node.treeName, node.treePath, page)
-            dock.addDockableSurface(localFile.dockable, localFile.surface)
-            val fileLeaf = dock.getLeafAtPath("0/1")
-            if (fileLeaf != null) fileLeaf.dock(localFile.dockable)
-            else dock.getLeafAtPath("0")?.insertItem(localFile.dockable, DockNode.SlotPosition.Right)
-            localFile
-        }
-    }
 }
+
 
