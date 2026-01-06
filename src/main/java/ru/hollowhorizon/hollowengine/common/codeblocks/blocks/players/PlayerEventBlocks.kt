d32e247703100289f60e1fda721d278a22af2ba6
HollowHorizon <artemm@xaker.ru>
2026-01-06 20:34:16 +0300

    Update PlayerEventBlocks.kt

--- src/main/java/ru/hollowhorizon/hollowengine/common/codeblocks/blocks/players/PlayerEventBlocks.kt
+++ src/main/java/ru/hollowhorizon/hollowengine/common/codeblocks/blocks/players/PlayerEventBlocks.kt
@@ -1,14 +1,15 @@
 package ru.hollowhorizon.hollowengine.common.codeblocks.blocks.players
 
-import de.fabmax.kool.modules.ui2.*
+import de.fabmax.kool.modules.ui2.AlignmentY
+import de.fabmax.kool.modules.ui2.Text
+import de.fabmax.kool.modules.ui2.alignY
+import de.fabmax.kool.modules.ui2.textColor
 import de.fabmax.kool.util.Color
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
 import net.minecraft.world.entity.player.Player
 import ru.hollowhorizon.hollowengine.client.gui.codeblocks.InputSlotScope
 import ru.hollowhorizon.hollowengine.common.codeblocks.model.StartBlock
-import ru.hollowhorizon.hollowengine.common.codeblocks.model.StatementBlock
 import ru.hollowhorizon.hollowengine.common.events.await
 import ru.hollowhorizon.hollowengine.common.events.entity.LivingEntityDeathEvent
 import ru.hollowhorizon.hollowengine.common.events.entity.player.PlayerEvent
@@ -33,13 +34,10 @@ class OnPlayerJoinBlock : StartBlock() {
 
 @Serializable
 @SerialName("hollowengine:events/player_death")
-class OnPlayerDeathBlock : StatementBlock(), StartBlock {
-    @Transient
-    override val isGlobal: MutableStateValue<Boolean> = mutableStateOf(false)
-
+class OnPlayerDeathBlock : StartBlock() {
     private val player by input<Player>("player")
 
-    override suspend fun execute() {
+    override suspend fun trigger() {
         while (true) {
             val event = await<LivingEntityDeathEvent>()
             if (event.entity == player()) return
