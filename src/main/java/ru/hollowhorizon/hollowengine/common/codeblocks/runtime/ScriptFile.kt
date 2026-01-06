f4a86cf4c5102a8a290cd53e4cbd555510d1d9eb
HollowHorizon <artemm@xaker.ru>
2026-01-06 20:24:19 +0300

    well, triggers are done?

--- src/main/java/ru/hollowhorizon/hollowengine/common/codeblocks/runtime/ScriptFile.kt
+++ src/main/java/ru/hollowhorizon/hollowengine/common/codeblocks/runtime/ScriptFile.kt
@@ -1,13 +1,16 @@
 package ru.hollowhorizon.hollowengine.common.codeblocks.runtime
 
+import kotlinx.coroutines.*
 import net.minecraft.nbt.CompoundTag
+import net.minecraft.nbt.ListTag
+import ru.hollowhorizon.hollowengine.HollowCore
 import ru.hollowhorizon.hollowengine.common.codeblocks.blocks.custom.CustomBlock
 import ru.hollowhorizon.hollowengine.common.codeblocks.blocks.variables.SetVarBlock
 import ru.hollowhorizon.hollowengine.common.codeblocks.createContainer
 import ru.hollowhorizon.hollowengine.common.codeblocks.model.BlockModel
 import ru.hollowhorizon.hollowengine.common.codeblocks.model.StartBlock
 import ru.hollowhorizon.hollowengine.common.codeblocks.walk
-import java.util.*
+import ru.hollowhorizon.hollowengine.common.coroutines.dispatcher
 import java.util.concurrent.CopyOnWriteArrayList
 
 class ScriptFile(
@@ -15,65 +18,85 @@ class ScriptFile(
     val path: String,
     val allBlocks: List<BlockModel>
 ) {
-    val localVariables = VariableMap()
+    private val declaredLocalVariables = allBlocks.flatMap { it.walk() }
+        .filterIsInstance<SetVarBlock>()
+        .associate { it.variableName to it.expressionType }
 
     val instances = CopyOnWriteArrayList<ScriptInstance>()
-
     val functions = allBlocks.filterIsInstance<CustomBlock>().associateBy { it.function }
 
-    init {
-        allBlocks.flatMap { it.walk() }
-            .filterIsInstance<SetVarBlock>()
-            .forEach {
-                if (!localVariables.contains(it.variableName)) {
-                    localVariables[it.variableName] = createContainer(it.expressionType)
-                }
-            }
-    }
+    private val scope = CoroutineScope(system.owner.dispatcher + SupervisorJob())
 
     fun startGlobalTriggers() {
         allBlocks.filterIsInstance<StartBlock>()
             .filter { it.isGlobal.value }
             .forEach { trigger ->
-                launchNewInstance(trigger)
+                monitorGlobalTrigger(trigger)
             }
     }
 
-    fun launchNewInstance(rootBlock: StartBlock): ScriptInstance {
+    private fun monitorGlobalTrigger(trigger: StartBlock) {
+        scope.launch {
+            while (isActive) {
+                try {
+                    trigger.trigger()
+
+                    launchNewInstance(trigger)
+                } catch (e: CancellationException) {
+                    throw e
+                } catch (e: Exception) {
+                    HollowCore.LOGGER.error("Error in global trigger monitor: $path", e)
+                    cancel("Error in global trigger monitor: $path")
+                }
+            }
+        }
+    }
+
+    private fun launchNewInstance(rootBlock: StartBlock): ScriptInstance {
         val instance = ScriptInstance(this, rootBlock)
+
+        declaredLocalVariables.forEach { (name, type) ->
+            if (!instance.localVariables.contains(name)) {
+                instance.localVariables[name] = createContainer(type)
+            }
+        }
+
         instances.add(instance)
         instance.start()
         return instance
     }
 
     fun stopAll() {
+        scope.cancel()
         instances.forEach { it.stop() }
         instances.clear()
     }
 
     fun serialize(tag: CompoundTag) {
-        tag.put("locals", CompoundTag().apply(localVariables::serialize))
-
-        val instancesTag = CompoundTag()
+        val instancesList = ListTag()
         instances.forEach { instance ->
-            instancesTag.put(instance.root.uuid.toString(), CompoundTag().apply(instance::serialize))
+            instancesList.add(CompoundTag().apply(instance::serialize))
         }
-        tag.put("instances", instancesTag)
+        tag.put("instances", instancesList)
     }
 
     fun deserialize(tag: CompoundTag) {
-        localVariables.deserialize(tag.getCompound("locals"))
+        val instancesList = tag.getList("instances", 10)
 
-        val instancesList = tag.getCompound("instances")
-        instancesList.allKeys.forEach { uuid ->
-            val instTag = instancesList.getCompound(uuid)
-            val rootId = UUID.fromString(uuid)
+        instancesList.forEach { it ->
+            val instTag = it as CompoundTag
+            val rootId = instTag.getUUID("rootBlockId")
             val root = allBlocks.find { b -> b.uuid == rootId } as? StartBlock
 
             if (root != null) {
                 val instance = ScriptInstance(this, root)
+                declaredLocalVariables.forEach { (name, type) ->
+                    instance.localVariables[name] = createContainer(type)
+                }
+
                 instance.deserialize(instTag)
                 instances.add(instance)
+
                 instance.resume()
             }
         }
--- src/main/java/ru/hollowhorizon/hollowengine/common/codeblocks/runtime/ScriptInstance.kt
+++ src/main/java/ru/hollowhorizon/hollowengine/common/codeblocks/runtime/ScriptInstance.kt
@@ -1,47 +1,95 @@
 package ru.hollowhorizon.hollowengine.common.codeblocks.runtime
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.nbt.ListTag
-import ru.hollowhorizon.hollowengine.HollowCore
 import ru.hollowhorizon.hollowengine.common.codeblocks.BlockFrame
 import ru.hollowhorizon.hollowengine.common.codeblocks.execution.BlockFrameStackElement
 import ru.hollowhorizon.hollowengine.common.codeblocks.execution.CodeBlockInterpreter
 import ru.hollowhorizon.hollowengine.common.codeblocks.model.StartBlock
 import ru.hollowhorizon.hollowengine.common.coroutines.dispatcher
-import kotlin.coroutines.cancellation.CancellationException
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
 
 class ScriptInstance(
     val ownerFile: ScriptFile,
-    rootBlock: StartBlock,
+    val rootBlock: StartBlock,
 ) {
-    val scriptContext = ScriptContextElement(this)
-    val blockStack = BlockFrameStackElement(this)
+    val localVariables = VariableMap()
+    val scope = CoroutineScope(ownerFile.system.owner.dispatcher + SupervisorJob())
 
-    val scope = CoroutineScope(ownerFile.system.owner.dispatcher + SupervisorJob() + scriptContext + blockStack)
-
-    val interpreter = CodeBlockInterpreter<Unit>(rootBlock)
-    val root get() = interpreter.root
+    private val executors = ConcurrentHashMap<UUID, ExecutionContext>()
 
     fun start() {
+        launchBlockChain(rootBlock, stackToRestore = null)
+
+        ownerFile.allBlocks.filterIsInstance<StartBlock>()
+            .filter { !it.isGlobal.value }
+            .forEach { monitorLocalTrigger(it) }
+    }
+
+    fun resume() {
+        val restoredExecutors = HashMap(executors)
+        executors.clear()
+
+        restoredExecutors.forEach { (triggerUuid, context) ->
+            val triggerBlock = ownerFile.allBlocks.find { it.uuid == triggerUuid } as? StartBlock
+            if (triggerBlock != null) {
+                launchBlockChain(triggerBlock, stackToRestore = context.stack)
+            }
+        }
+
+        ownerFile.allBlocks.filterIsInstance<StartBlock>()
+            .filter { !it.isGlobal.value }
+            .forEach { trigger ->
+                // Если этот триггер сейчас не выполняется (не был восстановлен), запускаем слушатель
+                if (!executors.containsKey(trigger.uuid)) {
+                    // Если мы перезапускаем скрипт, то наверное старые слушатели, которые завершились уже не нужны?
+                } else {
+                    monitorLocalTrigger(trigger)
+                }
+            }
+    }
+
+    private fun monitorLocalTrigger(trigger: StartBlock) {
         scope.launch {
-            try {
-                interpreter.execute()
-            } catch (_: CancellationException) {
-                // Скрипт остановлен
-            } catch (e: Exception) {
-                HollowCore.LOGGER.error("Error in script ${ownerFile.path}", e)
-            } finally {
-                cleanup()
+            while (isActive) {
+                try {
+                    withContext(ScriptContextElement(this@ScriptInstance)) {
+                        trigger.trigger()
+                    }
+                    executors[trigger.uuid]?.job?.cancelAndJoin()
+                    launchBlockChain(trigger, stackToRestore = null)
+                } catch (e: CancellationException) {
+                    throw e
+                } catch (e: Exception) {
+                    delay(1000)
+                }
             }
         }
     }
 
+
+    private fun launchBlockChain(startBlock: StartBlock, stackToRestore: BlockFrameStackElement?) {
+        val stackElement = stackToRestore ?: BlockFrameStackElement(this)
+        val interpreter = CodeBlockInterpreter<Unit>(startBlock)
+
+        val context = ScriptContextElement(this) + stackElement
+
+        val job = scope.launch(context) {
+            try {
+                interpreter.execute()
+            } finally {
+                executors.remove(startBlock.uuid)
+            }
+        }
+
+        executors[startBlock.uuid] = ExecutionContext(job, stackElement)
+    }
+
     fun stop() {
         scope.cancel()
+        cleanup()
     }
 
     private fun cleanup() {
@@ -49,20 +97,36 @@ class ScriptInstance(
     }
 
     fun serialize(tag: CompoundTag) {
-        val list = ListTag()
-        list.addAll(blockStack.frames.map { it.tag })
-        tag.put("stack", list)
+        tag.putUUID("rootBlockId", rootBlock.uuid)
+        tag.put("locals", CompoundTag().apply(localVariables::serialize))
+
+        val threadsTag = CompoundTag()
+        executors.forEach { (uuid, context) ->
+            val framesList = ListTag()
+            framesList.addAll(context.stack.frames.map { it.tag })
+            threadsTag.put(uuid.toString(), framesList)
+        }
+        tag.put("threads", threadsTag)
     }
 
     fun deserialize(tag: CompoundTag) {
-        val frames = tag.getList("stack", 10).map {
-            BlockFrame(it as CompoundTag)
+        localVariables.deserialize(tag.getCompound("locals"))
+
+        val threadsTag = tag.getCompound("threads")
+        threadsTag.allKeys.forEach { key ->
+            val uuid = UUID.fromString(key)
+            val framesList = threadsTag.getList(key, 10)
+
+            // Восстанавливаем стек
+            val stackElement = BlockFrameStackElement(this)
+            stackElement.frames.addAll(framesList.map { BlockFrame(it as CompoundTag) })
+
+            executors[uuid] = ExecutionContext(Job(), stackElement)
         }
-        blockStack.frames.clear()
-        blockStack.frames.addAll(frames)
     }
 
-    fun resume() {
-        start()
-    }
+    private data class ExecutionContext(
+        val job: Job,
+        val stack: BlockFrameStackElement,
+    )
 }
