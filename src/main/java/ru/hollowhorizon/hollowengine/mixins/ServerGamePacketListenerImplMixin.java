50b91cfc23db863d15831b6f291eef8a28953219
HollowHorizon <artemm@xaker.ru>
2026-02-28 17:13:08 +0300

    Update ServerGamePacketListenerImplMixin.java

--- src/main/java/ru/hollowhorizon/hollowengine/mixins/ServerGamePacketListenerImplMixin.java
+++ src/main/java/ru/hollowhorizon/hollowengine/mixins/ServerGamePacketListenerImplMixin.java
@@ -16,7 +16,8 @@ public abstract class ServerGamePacketListenerImplMixin {
     @Shadow
     public ServerPlayer player;
 
-    @Shadow
+    // Damn Mohist/Tenet, why did you remove the obfuscation from this certain method?!
+    @Shadow(aliases = {"detectRateSpam", "m_215251_"})
     protected abstract void detectRateSpam();
 
     @Inject(method = "broadcastChatMessage", at = @At(value = "HEAD"), cancellable = true)
