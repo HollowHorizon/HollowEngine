678b2d15a98f2d6a1371caa475928f1fc9736336
HollowHorizon <artemm@xaker.ru>
2026-05-08 19:42:08 +0300

    safe component loading & named args fix

--- runtime/src/main/java/ru/hollowhorizon/hollowengine/common/geary/snapshot/EntitySnapshot.kt
+++ runtime/src/main/java/ru/hollowhorizon/hollowengine/common/geary/snapshot/EntitySnapshot.kt
@@ -1,16 +1,19 @@
 package ru.hollowhorizon.hollowengine.common.geary.snapshot
 
-import kotlinx.serialization.Polymorphic
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
+import kotlinx.serialization.*
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.entity.Entity
+import ru.hollowhorizon.hollowengine.HollowEngine
 import ru.hollowhorizon.hollowengine.common.geary.api.Component
 import ru.hollowhorizon.hollowengine.common.geary.components.ComponentDescriptorRegistry
 import ru.hollowhorizon.hollowengine.common.geary.components.ComponentPersistencePolicy
 import ru.hollowhorizon.hollowengine.common.utils.nbt.ForResourceLocation
 import ru.hollowhorizon.hollowengine.common.utils.nbt.ForUuid
-import java.util.UUID
+import java.util.*
 
 @Serializable
 sealed class Snapshot {
@@ -28,6 +31,7 @@ sealed class Snapshot {
 
 @Serializable
 data class EntitySnapshot(
+    @Serializable(with = ComponentListSerializer::class)
     override val components: List<@Polymorphic Component> = emptyList(),
 ) : Snapshot() {
     @Transient
@@ -40,6 +44,32 @@ data class EntitySnapshot(
             ComponentDescriptorRegistry.descriptorOrNull(component::class)?.persistencePolicy == ComponentPersistencePolicy.LOOSE_ON_DEATH
         },
     )
+}
+
+object ComponentListSerializer : KSerializer<List<Component>> {
+    private val delegate = ListSerializer(PolymorphicSerializer(Component::class))
+
+    override val descriptor: SerialDescriptor = delegate.descriptor
+
+    override fun serialize(encoder: Encoder, value: List<Component>) {
+        delegate.serialize(encoder, value)
+    }
+
+    override fun deserialize(decoder: Decoder): List<Component> {
+        val result = mutableListOf<Component>()
+        val composite = decoder.beginStructure(descriptor)
+
+        val size = composite.decodeCollectionSize(descriptor)
+        for (i in 0 until size) {
+            try {
+                result.add(composite.decodeSerializableElement(descriptor, i, PolymorphicSerializer(Component::class)))
+            } catch (e: Exception) {
+                HollowEngine.LOGGER.info("Missing component: ${e.message}")
+            }
+        }
+        composite.endStructure(descriptor)
+        return result
+    }
 }
 
 @Serializable
