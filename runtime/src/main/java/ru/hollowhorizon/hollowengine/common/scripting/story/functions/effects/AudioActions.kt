14c913a9f62dc864299094fc1ba1e7b7b858993b
HollowHorizon <artemm@xaker.ru>
2026-05-10 19:43:55 +0300

    better sounds

--- runtime/src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/functions/effects/AudioActions.kt
+++ runtime/src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/functions/effects/AudioActions.kt
@@ -1,8 +1,13 @@
 package ru.hollowhorizon.hollowengine.common.scripting.story.functions.effects
 
 import kotlinx.serialization.Serializable
+import net.minecraft.core.Holder
+import net.minecraft.network.protocol.game.ClientboundSoundPacket
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.level.ServerPlayer
+import net.minecraft.sounds.SoundEvent
+import net.minecraft.sounds.SoundSource
+import net.minecraft.util.Mth
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.level.Level
 import net.minecraft.world.phys.Vec3
@@ -12,12 +17,13 @@ import ru.hollowhorizon.hollowengine.client.audio.Wave
 import ru.hollowhorizon.hollowengine.client.audio.formats.Mp3Format
 import ru.hollowhorizon.hollowengine.client.audio.formats.OggFormat
 import ru.hollowhorizon.hollowengine.client.audio.formats.WavFormat
+import ru.hollowhorizon.hollowengine.client.utils.stream
+import ru.hollowhorizon.hollowengine.common.network.HollowPacket
+import ru.hollowhorizon.hollowengine.common.network.HollowPacketHandler
 import ru.hollowhorizon.hollowengine.common.utils.nbt.ForResourceLocation
 import ru.hollowhorizon.hollowengine.common.utils.nbt.ForVec3
 import ru.hollowhorizon.hollowengine.common.utils.rl
-import ru.hollowhorizon.hollowengine.client.utils.stream
-import ru.hollowhorizon.hollowengine.common.network.HollowPacketHandler
-import ru.hollowhorizon.hollowengine.common.network.HollowPacket
+import kotlin.math.sqrt
 
 
 infix fun Level.playSound(location: String) = playSound(location, 1f, 1f)
@@ -41,7 +47,40 @@ fun Player.playSound(
     velocity: Vec3? = null,
     relative: Boolean = true,
 ) {
-    SoundEffectPacket(location.rl, volume, pitch, position, velocity, relative).send(this as ServerPlayer)
+    if (location.substringAfter(':').startsWith("audio") || location.endsWith(".mp3") || location.endsWith(".wav")) {
+        SoundEffectPacket(location.rl, volume, pitch, position, velocity, relative).send(this as ServerPlayer)
+    } else {
+        var pos = position ?: Vec3.ZERO
+        val holder = Holder.direct(SoundEvent.createVariableRangeEvent(location.rl))
+        val e: Double = pos.x - x
+        val f: Double = pos.y - y
+        val g: Double = pos.z - z
+        val h = e * e + f * f + g * g
+        var volume = volume
+        val d = Mth.square(holder.value().getRange(volume)).toDouble()
+        if (h > d) {
+            val k = sqrt(h)
+            pos = Vec3(
+                x + e / k * 2.0,
+                y + f / k * 2.0,
+                z + g / k * 2.0
+            )
+            volume = 0.1f
+        }
+        (this as ServerPlayer).connection.send(
+            ClientboundSoundPacket(
+                holder,
+                SoundSource.MASTER,
+                pos.x,
+                pos.y,
+                pos.z,
+                volume,
+                pitch,
+                level().random.nextLong()
+            )
+        )
+
+    }
 }
 
 val SOUNDS = HashMap<ResourceLocation, Wave>()
