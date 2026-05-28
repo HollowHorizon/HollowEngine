3fc0ff58558a65b54c50803d16f69fe75f4ef8e6
Nero <izhukov2007@gmail.com>
2026-05-28 20:09:20 +0700

    Update HollowEnginePack.kt

--- runtime/src/main/java/ru/hollowhorizon/hollowengine/common/data/HollowEnginePack.kt
+++ runtime/src/main/java/ru/hollowhorizon/hollowengine/common/data/HollowEnginePack.kt
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject
 import net.minecraft.SharedConstants
 import net.minecraft.server.packs.PackLocationInfo
 import net.minecraft.server.packs.PathPackResources
+import net.minecraft.server.packs.PackType
 import net.minecraft.server.packs.repository.PackSource
 import net.minecraft.server.packs.resources.IoSupplier
 import ru.hollowhorizon.hollowengine.common.events.SubscribeEvent
