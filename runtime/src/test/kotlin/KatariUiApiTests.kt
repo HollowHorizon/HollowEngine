d617f92011b65986f6f06692b7d929d401881824
HollowHorizon <artemm@xaker.ru>
2026-06-07 13:51:03 +0300

    remove outdated tests

--- runtime/src/test/kotlin/KatariUiApiTests.kt
+++ runtime/src/test/kotlin/KatariUiApiTests.kt
@@ -1,64 +1,10 @@
-import com.sunnychung.lib.multiplatform.kotlite.model.XML_TEXT_NODE_NAME
-import com.sunnychung.lib.multiplatform.kotlite.model.XML_TEXT_VALUE_ATTRIBUTE
-import ru.hollowhorizon.hollowengine.client.ui.BaseUiNode
-import ru.hollowhorizon.hollowengine.client.ui.TextNode
-import ru.hollowhorizon.hollowengine.client.ui.xml.UiXmlBuilder
 import ru.hollowhorizon.hollowengine.client.ui.xml.UiXmlTree
-import ru.hollowhorizon.hollowengine.client.ui.xml.parseUi
 import ru.hollowhorizon.hollowengine.common.scripting.katari.KatariScriptingAnalyzer
 import ru.hollowhorizon.hollowengine.common.scripting.katari.KatariUiDocument
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertIs
 
 class KatariUiApiTests {
-    @Test
-    fun `ui xml supports boxed text composition`() {
-        val root = parseUi(
-            """
-            <box>
-                <box id="continue">
-                    <text>Continue</text>
-                </box>
-            </box>
-            """.trimIndent(),
-        )
-
-        val container = assertIs<BaseUiNode>(root.children.single())
-        val text = assertIs<TextNode>(container.children.single())
-
-        assertEquals("box", container.type)
-        assertEquals("Continue", text.text.template)
-    }
-
-    @Test
-    fun `ui document inserts into tagged container`() {
-        val document = KatariUiDocument(
-            id = "test",
-            root = UiXmlTree("box", mapOf("tags" to "container")),
-        )
-
-        document.insertAt(
-            "container",
-            UiXmlTree(
-                "box",
-                children = listOf(
-                    UiXmlTree(
-                        "text",
-                        children = listOf(UiXmlTree(XML_TEXT_NODE_NAME, mapOf(XML_TEXT_VALUE_ATTRIBUTE to "Extra"))),
-                    ),
-                ),
-            ),
-        )
-
-        val root = UiXmlBuilder().build(document.root)
-        val container = assertIs<BaseUiNode>(root.children.single())
-        val text = assertIs<TextNode>(container.children.single())
-
-        assertEquals("box", container.type)
-        assertEquals("Extra", text.text.template)
-    }
-
     @Test
     fun `ui document modifies custom attributes on matching target`() {
         val document = KatariUiDocument(
@@ -97,7 +43,11 @@ class KatariUiApiTests {
                                 children = listOf(
                                     UiXmlTree(
                                         "text",
-                                        mapOf("id" to "dialog-message", "tags" to "message-text", "text" to "123456789"),
+                                        mapOf(
+                                            "id" to "dialog-message",
+                                            "tags" to "message-text",
+                                            "text" to "123456789"
+                                        ),
                                     ),
                                 ),
                             ),
